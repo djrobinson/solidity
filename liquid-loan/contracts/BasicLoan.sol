f39287d42a644746634229aa1a0800db73247892
Danny Robinson <daniel@djrobinson.me>
2018-06-17 23:16:58 -0600

    Loan pasted from remix

--- liquid-loan/contracts/BasicLoan.sol
+++ liquid-loan/contracts/BasicLoan.sol
@@ -3,28 +3,31 @@ pragma solidity ^0.4.0;
 contract Loan {
     uint public dateRequested;
     uint public dateActivated;
-    uint public requestedRateBps;
+    uint public requestedRate;
     uint public requestedAmount;
     address[] public contributors;
     mapping(address => uint) public contributions;
     uint public contributedAmount;
     uint public contributorCount;
-    uint public lengthInMonths;
+    uint public lengthInPeriods;
     uint public payment;
     uint public exponentAmount;
     uint public monthlyDivide;
     uint public oneBps;
+    uint public numerator;
+    uint public denominator;
 
 
-    constructor(uint _requestedRateBps, uint _requestedAmount, uint _lengthInMonths) public {
+    constructor(uint _requestedRate, uint _requestedAmount, uint _lengthInPeriods) public {
         dateRequested = now;
-        requestedRateBps = _requestedRateBps;
+        // Hard coding interest for now
+        // requestedRate = _requestedRate;
+        requestedRate = 20;
         requestedAmount = _requestedAmount;
-        lengthInMonths = _lengthInMonths;
-        monthlyDivide = (requestedAmount * requestedRateBps / lengthInMonths / 10000);
-        exponentAmount = (10000 + (requestedRateBps / lengthInMonths))**lengthInMonths;
-        oneBps = 10000**lengthInMonths;
-        payment = monthlyDivide * exponentAmount / (exponentAmount - oneBps);
+        lengthInPeriods = _lengthInPeriods;
+        numerator = fracExp(requestedAmount, requestedRate, lengthInPeriods, 20) / requestedRate * 100000000;
+        denominator = fracExp(100000000, requestedRate, lengthInPeriods, 20) - 100000000;
+        payment = numerator / denominator;
     }
 
     function contribute() public payable {
@@ -32,14 +35,31 @@ contract Loan {
         contributions[msg.sender] = msg.value;
         contributedAmount += msg.value;
         contributorCount++;
-        if (contributedAmount >= requestedAmount) {
-            uint excessAmount = contributedAmount - requestedAmount;
-            // TODO: Need to refund excess back
-            activate();
-        }
+        // if (contributedAmount >= requestedAmount) {
+        //     uint excessAmount = contributedAmount - requestedAmount;
+        //     // TODO: Need to refund excess back
+        //     activate();
+        // }
     }
 
     function activate() private {
         dateActivated = now;
+    }
+
+    // Computes `k * (1+1/q) ^ N`, with precision `p`. The higher
+    // the precision, the higher the gas cost. It should be
+    // something around the log of `n`.
+    // Much smaller values are sufficient to get a great approximation.
+    function fracExp(uint k, uint q, uint n, uint p) returns (uint) {
+      uint s = 0;
+      uint N = 1;
+      uint B = 1;
+      // Might need to adjust how we set % to fraction later by changing q
+      for (uint i = 0; i < p; ++i){
+        s += k * N / B / (q**i);
+        N  = N * (n-i);
+        B  = B * (i+1);
+      }
+      return s;
     }
 }
